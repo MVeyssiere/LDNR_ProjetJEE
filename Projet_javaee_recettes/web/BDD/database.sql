fa7c79d42cb40630d0361de00b7008e48b4f3f0f
MarineVe <marine.veyssiere31@gmail.com>
2019-05-13 10:59:14 +0200

    BDD: création tables commentaire et recette

--- Projet_javaee_recettes/web/BDD/database.sql
+++ Projet_javaee_recettes/web/BDD/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 13 Mai 2019 à 08:49
+-- Généré le :  Lun 13 Mai 2019 à 08:58
 -- Version du serveur :  5.5.59-0+deb8u1
 -- Version de PHP :  5.6.33-0+deb8u1
 
@@ -19,6 +19,37 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `2019devG1jee`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE IF NOT EXISTS `commentaire` (
+`id_commentaire` int(5) NOT NULL,
+  `titre` varchar(40) NOT NULL,
+  `corps` varchar(200) NOT NULL,
+  `signal` varchar(20) NOT NULL,
+  `date` date NOT NULL,
+  `FK_id_user` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recette`
+--
+
+CREATE TABLE IF NOT EXISTS `recette` (
+`id_recette` int(5) NOT NULL,
+  `titre` varchar(40) NOT NULL,
+  `votes_positifs` int(5) NOT NULL,
+  `votes_negatifs` int(5) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `FK_id_commentaire` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -39,6 +70,18 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 --
 
 --
+-- Index pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+ ADD PRIMARY KEY (`id_commentaire`);
+
+--
+-- Index pour la table `recette`
+--
+ALTER TABLE `recette`
+ ADD PRIMARY KEY (`id_recette`);
+
+--
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
@@ -48,6 +91,16 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+MODIFY `id_commentaire` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `recette`
+--
+ALTER TABLE `recette`
+MODIFY `id_recette` int(5) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
