d02ea5701db840ac762bfded9f2ca366bec620b3
MarineVe <marine.veyssiere31@gmail.com>
2019-05-14 22:25:31 +0200

    back et front: possibilité de créer une nouvelle recette. Enregistrement en BDD. Affichage des erreurs ou réussite.

---
+++ Projet_javaee_recettes/src/java/forms/creationRecetteForm.java
@@ -0,0 +1,124 @@
+package forms;
+
+import DAO.DAORecette;
+import beans.Recette;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Marine V
+ */
+public class creationRecetteForm {
+
+    public creationRecetteForm() {
+    }
+
+    private static final String TITRE = "titre";
+    private static final String INGREDIENTS = "ingredients";
+    private static final String RECETTE = "recette";
+
+    DAORecette daorecette = new DAORecette();
+
+    private String result;
+    private final Map<String, String> errors = new HashMap<>();
+
+    public String getResult() {
+        return result;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    private void setErrors(String field, String message) {
+        errors.put(field, message);
+    }
+
+    public Recette createRecette(HttpServletRequest request) {
+
+        String titre = getParamValue(request, TITRE);
+        String ingred = getParamValue(request, INGREDIENTS);
+        String recette = getParamValue(request, RECETTE);
+
+        System.out.println("get titre: " + titre);
+        System.out.println("get ingredients: " + ingred);
+        System.out.println("get recette: " + recette);
+
+        Recette newRecette = new Recette();
+
+        try {
+            validateTitle(titre);
+        } catch (Exception e) {
+            setErrors(TITRE, e.getMessage());
+        }
+
+        try {
+            validateIngredients(INGREDIENTS);
+        } catch (Exception e) {
+            setErrors(INGREDIENTS, e.getMessage());
+        }
+
+        try {
+            validateRecette(recette);
+        } catch (Exception e) {
+            setErrors(RECETTE, e.getMessage());
+        }
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar cal = Calendar.getInstance();
+        String dateDuJour = dateFormat.format(cal.getTime());
+
+        System.out.println("date du jour: " + dateDuJour);
+        /* Initialisation du résultat global de la validation. */
+        if (errors.isEmpty() && !daorecette.verifyTitle(titre)) {
+            newRecette.setTitre(titre);
+            newRecette.setIngredients(ingred);
+            newRecette.setDescription(recette);
+            newRecette.setDate(dateDuJour);
+            daorecette.create(newRecette);
+            System.out.println(newRecette.toString());
+            result = "Votre recette est enregistrée!";
+        } else if (daorecette.verifyTitle(titre)) {
+            setErrors(TITRE, "");
+            result = "Cette recette existe déjà.";
+        } else {
+            System.out.println("errors: " + errors);
+            setErrors(TITRE, "");
+            result = "Une erreur est survenue à l'enregistrement.";
+        }
+        return newRecette;
+
+    }
+
+    // retourne null si le champ est vide ou null, ou sa valeur « trimée » sinon, ce qui permet de simplifier les tests.
+    private static String getParamValue(HttpServletRequest request, String paramKey) {
+        String value = request.getParameter(paramKey);
+        if (value == null || value.trim().length() == 0) {
+            return null;
+        } else {
+            return value.trim();
+        }
+    }
+
+    private void validateRecette(String recette) throws Exception {
+        if (recette.isEmpty()) {
+            throw new Exception("Renseignez la recette");
+        }
+    }
+
+    private void validateIngredients(String ingredients) throws Exception {
+        if (ingredients.isEmpty()) {
+            throw new Exception("Renseignez les ingrédients de votre recette");
+        }
+    }
+
+    private void validateTitle(String title) throws Exception {
+        if (title.isEmpty()) {
+            throw new Exception("Quel nom donnez-vous à votre plat?");
+        }
+    }
+}
--- Projet_javaee_recettes/web/WEB-INF/creationRecette.jsp
+++ Projet_javaee_recettes/web/WEB-INF/creationRecette.jsp
@@ -16,31 +16,39 @@
         <title>Création d'une recette</title>
     </head>
     <body>
-        <jsp:include page="header.html"/>
+        <jsp:include page="../header.html"/>
         <div class="row">
-        <h1 class="text-info col-md-4 offset-md-4">Création d'une recette</h1>
+        <h1 class="text-info col-md-4 offset-md-4">Création d'une recette</h1>    
+        
+        <div class="${(empty form.errors) ? 'p-3 mb-2 bg-success text-white' : 'p-3 mb-2 bg-danger text-white'}"> ${form.result} </div>
+        <%--<p class="${(empty form.errors) ? 'success' : 'error'}"> ${form.result} </p>--%>
         </div>
-        <form>
+        <form method="post" action="creationRecette">
         <div class="row">
             <section class="form-group col-md-4 offset-md-4">
-                <label contentEditable role='textbox' aria-multiline='true' for="titre">Titre </label><input id="titre" type="text" class="form-control">
+                <label contentEditable role='textbox' aria-multiline='true' for="titre">Titre </label>
+                <input id="titre" name = "titre" type="text" class="form-control" value="<c:out value = "${createRecette.titre}" />">
+                <span class="error">${form.errors['titre']}</span>
             </section>
         </div>
         <div class="row">
             <section class="form-group col-md-4 offset-md-4">
                 <label for="ingredients">Ingrédients</label>
-                <textarea class="form-control" rows="5" id="ingredients"></textarea>
+                <textarea class="form-control" rows="5" id="ingredients" name ="ingredients" ><c:out value = "${createRecette.ingredients}" /></textarea>                
+                <span class="error">${form.errors['ingredients']}</span>
                 <!--<input id="ingredients" type="textarea" class="form-control">-->
             </section>
         </div>
         <div class="row">
             <section class="form-group col-md-4 offset-md-4">
                 <label for="recette">Recette</label>                
-                <textarea class="form-control" rows="6" id="recette"></textarea>
+                <textarea class="form-control" rows="6" id="recette" name="recette"><c:out value = "${createRecette.description}" /></textarea>
+                <span class="error">${form.errors['description']}</span>
                 <!--<input id="recette" type="textarea" class="form-control">-->
             </section>
         </div>
-        <button type="button" class="btn btn-warning col-md-4 offset-md-4" id="envoyer">Enregistrer</button>
+        <button type="submit" class="btn btn-warning col-md-4 offset-md-4">Enregistrer</button>
+        <!--<button type="button" class="btn btn-warning col-md-4 offset-md-4" id="envoyer">Enregistrer</button>-->
     </form>
        
     <%--<jsp:include page="footer.html"/>--%>
